152541c5d27000a6ee407c560f463a77cc83599b
ben <benben@benben.work>
2024-09-28 23:42:50 +0800

    Changes

---
+++ cmake-build-debug/CMakeFiles/c2-1-1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c2-1-1.dir/School_Test/c2/c2-1-1.c.o"
+  "CMakeFiles/c2-1-1.dir/School_Test/c2/c2-1-1.c.o.d"
+  "CMakeFiles/c2-1-1.dir/School_Test/c2/c2-1-13.c.o"
+  "CMakeFiles/c2-1-1.dir/School_Test/c2/c2-1-13.c.o.d"
+  "CMakeFiles/c2-1-1.dir/School_Test/c2/c2-1-7.c.o"
+  "CMakeFiles/c2-1-1.dir/School_Test/c2/c2-1-7.c.o.d"
+  "c2-1-1"
+  "c2-1-1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c2-1-1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
