2b4d35bb6e48c028a7c647ac87e74d47771caec8
jiamingxu <xujiaming@benben.work>
2024-11-05 17:41:27 +0800

    Changes

---
+++ cmake-build-debug/CMakeFiles/c6-2-10.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c6-2-10.dir/School_Test/c6/c6-2-10.c.o"
+  "CMakeFiles/c6-2-10.dir/School_Test/c6/c6-2-10.c.o.d"
+  "c6-2-10"
+  "c6-2-10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c6-2-10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
