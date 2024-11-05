2cf75700e7ff8f303325ef55b9f27cf593d6d411
benben-xu <benbenben2022@163.com>
2024-11-05 18:01:20 +0800

    Changes

---
+++ cmake-build-debug/CMakeFiles/Yanghui.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Yanghui.dir/Self-Learn/chapter02/Yanghui.c.o"
+  "CMakeFiles/Yanghui.dir/Self-Learn/chapter02/Yanghui.c.o.d"
+  "Yanghui.exe"
+  "Yanghui.pdb"
+  "libYanghui.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Yanghui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/c6-2-3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c6-2-3.dir/School_Test/c6/c6-2-3.c.o"
+  "CMakeFiles/c6-2-3.dir/School_Test/c6/c6-2-3.c.o.d"
+  "c6-2-3.exe"
+  "c6-2-3.pdb"
+  "libc6-2-3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c6-2-3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
